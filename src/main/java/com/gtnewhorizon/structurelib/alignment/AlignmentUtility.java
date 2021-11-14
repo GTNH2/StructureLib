eb0d9bfd8ffba61a13f390bcd244be001e4e21a9
Tec <daniel112092@gmail.com>
2021-11-14 21:06:53 +0100

    Skew6ABC Cleanup debug code

--- src/main/java/com/gtnewhorizon/structurelib/alignment/AlignmentUtility.java
+++ src/main/java/com/gtnewhorizon/structurelib/alignment/AlignmentUtility.java
@@ -2,21 +2,15 @@ package com.gtnewhorizon.structurelib.alignment;
 
 import com.gtnewhorizon.structurelib.alignment.enumerable.ABCDirection;
 import com.gtnewhorizon.structurelib.alignment.enumerable.ExtendedFacing;
-import com.gtnewhorizon.structurelib.structure.StructureDefinition;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.common.util.ForgeDirection;
 
-import javax.annotation.Nullable;
-
 import static com.gtnewhorizon.structurelib.alignment.Skew.LEN;
 import static com.gtnewhorizon.structurelib.alignment.Skew.NONE;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlock;
 
 public class AlignmentUtility {
     private AlignmentUtility(){
@@ -41,48 +35,8 @@ public class AlignmentUtility {
         return false;
     }
 
-    static TA ta=new TA();
-    private static class TA extends TileEntity implements IAlignmentProvider{
-        public Skew skew=NONE;
-
-        @Nullable
-        @Override
-        public IAlignment getAlignment() {
-            return new IAlignment() {
-                @Override
-                public ExtendedFacing getExtendedFacing() {
-                    return ExtendedFacing.DEFAULT;
-                }
-
-                @Override
-                public void setExtendedFacing(ExtendedFacing alignment) {
-
-                }
-
-                @Override
-                public IAlignmentLimits getAlignmentLimits() {
-                    return UNLIMITED_AND_SKEW;
-                }
-
-                @Override
-                public void setSkew(Skew skew) {
-                    TA.this.skew=skew;
-                }
-
-                @Override
-                public Skew getSkew() {
-                    return skew;
-                }
-            };
-        }
-    }
-
-    //todo
     public static boolean handleSkew(EntityPlayer aPlayer, World aWorld, int aX, int aY, int aZ, int side, float hitX, float hitY, float hitZ){
         TileEntity tTileEntity = aWorld.getTileEntity(aX, aY, aZ);
-        if(tTileEntity==null){
-            tTileEntity=ta;
-        }
         if (tTileEntity == null || aPlayer instanceof FakePlayer) {
             return aPlayer instanceof EntityPlayerMP;
         }
@@ -129,15 +83,6 @@ public class AlignmentUtility {
                             throw new RuntimeException("I like unreachable code.");
                     }
                     alignment.toolSetSkew(alignment.getSkew().withAdditionalSkew(skews));
-
-                    StructureDefinition.builder().addShape("main",new String[][]{
-                            {"01110","11111", "11~11", "11111","01110",},
-                            {"11111","11111", "11111", "11111","11111",},
-                            {"11111","11111", "11011", "11111","11111",},
-                            {"11111","11111", "11111", "11111","11111",},
-                            {"01110","11111", "11111", "11111","01110",},
-                    }).addElement('1', ofBlock(Blocks.stone, 0)).addElement('0', ofBlock(Blocks.coal_block, 0)).build().hints(
-                            null,null,"main", Minecraft.getMinecraft().theWorld,ExtendedFacing.DEFAULT, ta.skew, aX,aY,aZ,2,2,0);
                 }
                 return true;
             }
