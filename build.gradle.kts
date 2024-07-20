263b174beda23b88c647a4c8e4a1bd2f74d52f01
FalsePattern <me@falsepattern.com>
2024-07-20 19:06:40 +0200

    migrate to new buildscript

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("fpgradle-minecraft") version ("0.6.1")
+}
+
+group = "mega"
+
+minecraft_fp {
+    mod {
+        modid = "structurelib"
+        name = "StructureLib"
+        rootPkg = "com.gtnewhorizon.structurelib"
+    }
+
+    tokens {
+        tokenClass = "Tags"
+        modid = "MODID"
+        name = "MODNAME"
+        version = "VERSION"
+        rootPkg = "GROUPNAME"
+    }
+
+    publish {
+        maven {
+            repoUrl = "https://mvn.falsepattern.com/gtmega_releases/"
+            repoName = "mega"
+        }
+    }
+}
+
+repositories {
+    mega()
+}
+
+dependencies {
+    runtimeOnlyNonPublishable("codechicken:notenoughitems-mc1.7.10:2.3.1-mega:dev")
+    runtimeOnlyNonPublishable("codechicken:codechickencore-mc1.7.10:1.4.0-mega:dev")
+
+    devOnlyNonPublishable("mega:carpentersblocks-mc1.7.10:3.4.1-mega:dev")
+}
